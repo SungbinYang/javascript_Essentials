d289fc8c7f51073e91f8e52b233b15a4b826c1e5
Robert.Yang <ysb05222@naver.com>
2021-05-27 22:37:51 +0900

    산술, 할당 연산자
    산술, 할당 연산자

--- main.js
+++ main.js
@@ -12,3 +12,19 @@ console.log(getType(false)); // Boolean
 console.log(getType(null)); // Null
 console.log(getType({})); // Object
 console.log(getType([])); // Array
+
+// 산술 연산자(arithmetic operator)
+
+console.log(1 + 2);
+console.log(5 - 7);
+console.log(3 * 4);
+console.log(10 / 2);
+console.log(7 % 5);
+
+// 할당 연산자(assignment operator)
+
+let a = 2; // const는 재할당 불가
+// a = a + 1;
+a += 1;
+
+console.log(a);
