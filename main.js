f808c05bca5e0ae12c016d3dc4064910c595106b
Robert.Yang <ysb05222@naver.com>
2021-05-27 22:57:42 +0900

    비교, 논리연산자
    비교, 논리연산자

--- main.js
+++ main.js
@@ -28,3 +28,41 @@ let a = 2; // const는 재할당 불가
 a += 1;
 
 console.log(a);
+
+// 비교 연산자(comparison operaotr)
+
+const a1 = 1;
+const b1 = 3;
+
+console.log(a1 === b1);
+
+// 비교 함수
+function isEqual(x, y) {
+    return x === y;
+}
+
+console.log(isEqual(1, 1));
+console.log(isEqual(2, '2'));
+
+const a2 = 13;
+const b2 = 13;
+
+console.log(a2 !== b2);
+console.log(a2 < b2);
+console.log(a2 > b2);
+console.log(a2 >= b2);
+console.log(a2 <= b2);
+
+// 논리 연산자(logical operator)
+
+const a3 = 1 === 123;
+const b3 = 'AB' === 'ABC';
+const c = false;
+
+console.log(a3);
+console.log(b3);
+console.log(c);
+
+console.log('&&: ', a3 && c);
+console.log('||: ', a3 || b3 || c);
+console.log('!: ', !a3);
